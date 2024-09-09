44f80107f1fa3944f50b5fa9f191cb274bbfc4fa
agnesnora <agnesnora@gmail.com>
2024-09-09 14:38:32 +0200

    setting up useWindowSize customhook

--- components/Navbar.jsx
+++ components/Navbar.jsx
@@ -1,14 +1,21 @@
+"use client";
+import styles from "../styles/Navbar.module.scss";
 import React from "react";
 import Link from "next/link";
 import { FaGoogle } from "react-icons/fa";
+import { useState } from "react";
+import useWindowSize from "@/hooks/useWindowSize";
 
 const Navbar = () => {
+  useWindowSize();
+  console.log(window.innerWidth);
+  const [isMobileMenuOn, setIsMobileMenuOn] = useState(false);
   return (
     <div>
       <Link href="/">Home Page</Link>
       <Link href="/chickens">Go to chickens</Link>
       <Link href="/chickens/add">Add chickens</Link>
-      <button>
+      <button className={styles.title}>
         {" "}
         <FaGoogle />
         Log in or register
