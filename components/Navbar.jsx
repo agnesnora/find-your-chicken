f43bb66c5d7e2629ee23dfb997e6b2a62f92c045
agnesnora <agnesnora@gmail.com>
2024-09-10 10:33:27 +0200

    setting up dropdown menu toggle on mobile screen

--- components/Navbar.jsx
+++ components/Navbar.jsx
@@ -1,20 +1,51 @@
 "use client";
 import styles from "../styles/Navbar.module.scss";
-import React from "react";
+import React, { useEffect } from "react";
 import Link from "next/link";
 import { FaGoogle } from "react-icons/fa";
+import { FiMenu } from "react-icons/fi";
 import { useState } from "react";
 import useWindowSize from "@/hooks/useWindowSize";
 
 const Navbar = () => {
-  useWindowSize();
-  console.log(window.innerWidth);
+  const { width } = useWindowSize();
   const [isMobileMenuOn, setIsMobileMenuOn] = useState(false);
+
+  //Close the mobile menu if window width is >700
+
+  useEffect(() => {
+    if (width > 700) {
+      setIsMobileMenuOn(false);
+    }
+  }, [width]);
+
   return (
-    <div>
-      <Link href="/">Home Page</Link>
-      <Link href="/chickens">Go to chickens</Link>
-      <Link href="/chickens/add">Add chickens</Link>
+    <div className={styles.navbar}>
+      {/* Mobile menu toggle icon*/}
+      {width < 700 && (
+        <FiMenu onClick={() => setIsMobileMenuOn((prev) => !prev)} />
+      )}
+      {/* Mobile menu dropdown */}
+
+      {width < 700 && isMobileMenuOn && (
+        <div className={styles.mobileMenu}>
+          {" "}
+          <Link href="/">Home Page</Link>
+          <Link href="/chickens">Go to chickens</Link>
+          <Link href="/chickens/add">Add chickens</Link>
+        </div>
+      )}
+      {/* Wide screen menu */}
+
+      {width > 700 && (
+        <div className={styles.mainMenu}>
+          {" "}
+          <Link href="/">Home Page</Link>
+          <Link href="/chickens">Go to chickens</Link>
+          <Link href="/chickens/add">Add chickens</Link>
+        </div>
+      )}
+
       <button className={styles.title}>
         {" "}
         <FaGoogle />
