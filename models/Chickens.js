749b2bc2f2a4fa4a3ffdb9fe6c54014daa2edd17
agnesnora <agnesnora@gmail.com>
2024-09-11 15:02:56 +0200

    setting up data, cloudinary

---
+++ models/Chickens.js
@@ -0,0 +1,64 @@
+import { Schema, model, models } from "mongoose";
+
+const ChickenSchema = new Schema(
+  {
+    owner: {
+      type: Schema.Types.ObjectId,
+      ref: "User",
+      required: true,
+    },
+    name: {
+      type: String,
+      required: true,
+    },
+    type: {
+      type: String,
+      required: true,
+    },
+    description: {
+      type: String,
+    },
+    location: {
+      street: {
+        type: String,
+      },
+      city: {
+        type: String,
+      },
+
+      zipcode: {
+        type: String,
+      },
+    },
+    rate: {
+      type: Number,
+    },
+    seller_info: {
+      name: {
+        type: String,
+      },
+      email: {
+        type: String,
+      },
+      phone: {
+        type: String,
+      },
+    },
+    images: [
+      {
+        type: String,
+      },
+    ],
+    is_featured: {
+      type: Boolean,
+      default: false,
+    },
+  },
+  {
+    timestamps: true,
+  }
+);
+
+const Chicken = models.Chicken || model("Chicken", ChickenSchema);
+
+export default Chicken;
